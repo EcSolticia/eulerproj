5430ce6f29fb3657f06d3de216cc3b0adb41a612
EcSolticia <quinnecsolticia@gmail.com>
2025-02-20 01:32:37 +0600

    Improved `is_prime_naive` to reduce problem7 solving time from 8.75 seconds to 0.07 seconds avg

--- include/mpzhelpers.h
+++ include/mpzhelpers.h
@@ -5,12 +5,21 @@
 void mpz_increment(mpz_t number) {
     mpz_add_ui(number, number, 1);
 }
+void mpz_double_increment(mpz_t number) {
+    mpz_add_ui(number, number, 2);
+}
 
 bool is_prime_naive(mpz_t number) {
+
     mpz_t i;
     mpz_init(i);
     mpz_set_ui(i, 2);
-    while (mpz_cmp(number, i) > 0) {
+
+    mpz_t f;
+    mpz_init(f);
+    mpz_sqrt(f, number);
+    mpz_increment(f);
+    while (mpz_cmp(f, i) > 0) {
         if (mpz_divisible_p(number, i)) {
             return false;
         }
--- problem7.c
+++ problem7.c
@@ -1,18 +1,21 @@
 #include "include/mpzhelpers.h"
+#include <time.h>
 
 int main() {
 
+    clock_t begin = clock();
+
     mpz_t counter;
     mpz_init(counter);
-    mpz_set_ui(counter, 1);
+    mpz_set_ui(counter, 5);
 
     mpz_t prime_counter;
     mpz_init(prime_counter);
-    mpz_set_ui(counter, 1);
+    mpz_set_ui(prime_counter, 3);
 
     mpz_t last_prime;
     mpz_init(last_prime);
-    mpz_set_ui(last_prime, 1);
+    mpz_set_ui(last_prime, 3);
 
     while (mpz_cmp_ui(prime_counter, 10002) < 0) {
 
@@ -21,10 +24,17 @@ int main() {
             mpz_set(last_prime, counter);
         }
 
-        mpz_increment(counter);
+        mpz_double_increment(counter);
     } 
 
     gmp_printf("The 10001th prime is %Zd\n", last_prime);
 
+    mpz_clear(counter);
+    mpz_clear(prime_counter);
+    mpz_clear(last_prime);
+    
+    clock_t end = clock();
+    double time_spent = (double)(end - begin)/ CLOCKS_PER_SEC;
+    printf("\n%f\n", time_spent);
     return 0;
 }
